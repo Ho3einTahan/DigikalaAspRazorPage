b08b411ff8c0f24d181056c2067f5d28ef8ed098
ho3eintahan <hoseintahan84@gmail.com>
2025-01-04 09:04:54 +0330

    Update configuration

---
+++ Pages/Dashboard/user.cshtml.cs
@@ -0,0 +1,72 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+[Authorize(Roles = "Admin")]
+public class UserModel : PageModel
+{
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+
+    public UserModel(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+    {
+        _userManager = userManager;
+        _roleManager = roleManager;
+    }
+
+    public List<UserWithRoles> Users { get; set; }
+    public List<string> AllRoles { get; set; }
+
+    public async Task OnGetAsync()
+    {
+        var users = _userManager.Users.ToList();
+
+        Users = new List<UserWithRoles>();
+
+        foreach (var user in users)
+        {
+            var roles = await _userManager.GetRolesAsync(user);
+
+            Users.Add(new UserWithRoles
+            {
+                Id = user.Id,
+                Username = user.UserName,
+                Roles = roles.ToList()
+            });
+        }
+
+        AllRoles = _roleManager.Roles.Select(r => r.Name).ToList();
+    }
+
+    public async Task<IActionResult> OnPostAsync(string UserId, string SelectedRole)
+    {
+        var user = await _userManager.FindByIdAsync(UserId);
+        if (user != null)
+        {
+            await _userManager.AddToRoleAsync(user, SelectedRole);
+        }
+
+        return RedirectToPage();
+    }
+
+    public async Task<IActionResult> OnPostRemoveRoleAsync(string UserId, string RoleToRemove)
+    {
+        var user = await _userManager.FindByIdAsync(UserId);
+
+        if (user != null)
+        {
+            await _userManager.RemoveFromRoleAsync(user, RoleToRemove);
+        }
+
+        return RedirectToPage();
+    }
+
+    public class UserWithRoles
+    {
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public List<string> Roles { get; set; }
+    }
+
+}
