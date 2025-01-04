970f1437dd676c5326bb20fdf8159fb0ede135a3
ho3eintahan <hoseintahan84@gmail.com>
2025-01-04 08:55:05 +0330

    Add Migrations File

---
+++ Migrations/20250103060423_InitialIdentitySetup1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace digikala_netCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialIdentitySetup1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
